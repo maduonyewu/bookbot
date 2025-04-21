30568e880191cf512b8ede608dded2acf5c4683a
maduonyewu <madu.onyewu@gmail.com>
2025-04-20 22:12:17 -0400

    Initial book reporting complete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count, sort_dict
 
 def get_book_text(fp):
     txt = ""
@@ -10,6 +10,19 @@ def get_book_text(fp):
 def main():
     text = get_book_text("./books/frankenstein.txt")
     word_count = get_word_count(text)
-    print(f"{word_count} words found in the document")
+    #print(f"{word_count} words found in the document")
+    char_count = get_character_count(text)
+    print(char_count)
+    print("============ BOOKBOT ============")
+    print(f"----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    sorted_char_count = sort_dict(char_count)
+    for k,v in sorted_char_count.items():
+        if k.isalpha():
+            print(f"{k}: {v}")
+    print("============= END ===============")
+    #print(sort_dict(char_count))
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def get_word_count(text):
     count = len(text.split())
     return count
+
+def get_character_count(text):
+    count = {}
+    for c in text:
+        c = c.lower()
+        if c not in count:
+            count[c] = 0
+        count[c] += 1
+    return count
+
+def sort_dict(dictionary):
+    sorted_dict = sorted(dictionary.items(), reverse=True, key=lambda item: item[1])
+    return dict(sorted_dict)
