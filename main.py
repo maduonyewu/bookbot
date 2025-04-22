f07d0fd7267fba556c8440b52b8bab4f5b3eae61
maduonyewu <madu.onyewu@gmail.com>
2025-04-21 23:25:30 -0400

    Added variable filepath using sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, get_character_count, sort_dict
+import sys
 
 def get_book_text(fp):
     txt = ""
@@ -8,7 +9,11 @@ def get_book_text(fp):
 
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    #text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     word_count = get_word_count(text)
     #print(f"{word_count} words found in the document")
     char_count = get_character_count(text)
